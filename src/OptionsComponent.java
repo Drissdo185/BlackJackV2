d3eb50fd08431d178e9da44b312bde95b86322a9
Ngô Hoàng Thành <143169967+Ngo-Thanh@users.noreply.github.com>
2023-12-11 09:26:13 +0700

    Update OptionsComponent.java
    add sound effect demo

--- src/OptionsComponent.java
+++ src/OptionsComponent.java
@@ -8,17 +8,20 @@ import java.io.*;
 
 public class OptionsComponent extends JComponent implements ActionListener{
 
+    public BufferedImage PlayButtonHighlight;
+    public BufferedImage HelpButton;
+    public BufferedImage QuitButton;
+
     private JButton btnPlay = new JButton("PLAY");
     private JButton btnExit = new JButton("EXIT");
-    private JButton btnHelp = new JButton("HELP");
+    private JButton btnRule = new JButton("RULE");
     private JButton btnInfo = new JButton("INFO");
-    private JButton btnStart = new JButton("Start");
     private static BufferedImage backgroundImage, logo;
 
     public OptionsComponent() {
         btnPlay.addActionListener(this);
         btnExit.addActionListener(this);
-        btnHelp.addActionListener(this);
+        btnRule.addActionListener(this);
         btnInfo.addActionListener(this);
     }
 
@@ -28,30 +31,30 @@ public class OptionsComponent extends JComponent implements ActionListener{
 
         try {
             backgroundImage = ImageIO.read(new File("images/background.png"));
-            logo = ImageIO.read(new File("src/logo.png"));
+            logo = ImageIO.read(new File("images/logo.png"));
         }
         catch(IOException e) {}
 
         g2.drawImage(backgroundImage, 0, 0, null);
-        g2.drawImage(logo, 390, 100, null);
+        g2.drawImage(logo, 220, 20, null);
 
         g2.setColor(Color.WHITE);
         g2.setFont(new Font("Arial", Font.BOLD, 30));
-        g2.drawString("Chơi đi, muốn làm giàu thì chơi đi", 220, 580);
+        g2.drawString("cờ bạc, người không chơi là người thắng!", 270, 580);
 
-        btnPlay.setBounds(500, 300, 150, 80);
-        btnExit.setBounds(500, 400, 150, 80);
-        btnHelp.setBounds(80, 75, 150, 80);
-        btnInfo.setBounds(900, 75, 150, 80);
+        btnPlay.setBounds(370, 350, 150, 60);
+        btnExit.setBounds(370, 450, 150, 60);
+        btnRule.setBounds(610, 350, 150, 60);
+        btnInfo.setBounds(610, 450, 150, 60);
 
-        btnPlay.setFont(new Font("Comic Sans MS", Font.BOLD, 40));
-        btnExit.setFont(new Font("Comic Sans MS", Font.BOLD, 40));
-        btnHelp.setFont(new Font("Comic Sans MS", Font.BOLD, 40));
-        btnInfo.setFont(new Font("Comic Sans MS", Font.BOLD, 40));
+        btnPlay.setFont(new Font("Times New Roman", Font.BOLD, 36));
+        btnExit.setFont(new Font("Times New Roman", Font.BOLD, 36));
+        btnRule.setFont(new Font("Times New Roman", Font.BOLD, 36));
+        btnInfo.setFont(new Font("Times New Roman", Font.BOLD, 36));
 
         super.add(btnPlay);
         super.add(btnExit);
-        super.add(btnHelp);
+        super.add(btnRule);
         super.add(btnInfo);
     }
 
@@ -59,38 +62,45 @@ public class OptionsComponent extends JComponent implements ActionListener{
         JButton selectedButton = (JButton)e.getSource();
 
         if(selectedButton == btnExit) {
-            playSE(".//restest//click.wav");
+            playSE(".//res//chips.wav");
             System.exit(0);
         }
         else if(selectedButton == btnPlay) {
+            playSE(".//res//chips.wav");
             Tester.currentState = Tester.STATE.GAME;
             Tester.menuFrame.dispose();
             Tester.gameRefreshThread.start();
             Tester.gameCheckThread.start();
             //playAmbienceMusic();
-            playSE(".//res//nutamthanh.wav");
         }
-        else if(selectedButton == btnHelp) {
-            playSE(".//res//click.wav");
-            JOptionPane.showMessageDialog(this, "The goal of blackjack is to beat the dealer's hand without going over 21." +
-                            "\nFace cards are worth 10. Aces are worth 1 or 11, whichever makes a better hand." +
-                            "\nEach player starts with two cards, one of the dealer's cards is hidden until the end." +
-                            "\nTo 'Hit' is to ask for another card. To 'Stand' is to hold your total and end your turn." +
-                            "\nIf you go over 21 you bust, and the dealer wins regardless of the dealer's hand." +
-                            "\nIf you go over 21 you bust, and the dealer wins regardless of the dealer's hand." +
-                            "\nIf you are dealt 21 from the start (Ace & 10), you got a blackjack.", "QUICK&EASY BLACKJACK HELP",
+        else if(selectedButton == btnRule) {
+            playSE(".//res//chips.wav");
+            JOptionPane.showMessageDialog(this, "1. GOAL: have a hand value closer to 21 than the dealer's hand without exceeding 21." +
+                            "\n2. CARD VALUES:" +
+                            "\n   - Face cards (King, Queen, Jack) are each worth 10 points." +
+                            "\n   - Aces can be worth either 1 or 11 points, depending on which value benefits the hand more." +
+                            "\n3. THE DEAL: You are dealt two cards each, and the dealer receives one card face up and one face down (hole card)." +
+                            "\n4. TURN:" +
+                            "\n   - You can choose to \"hit\" (take another card) or \"stand\" (keep your current hand)." +
+                            "\n   - You can continue to hit until you decide to stand, or until your hand exceeds 21, resulting in a bust." +
+                            "\n5. WINNING & LOSING:" +
+                            "\n   - You win if your hand is closer to 21 than the dealer's hand without busting." +
+                            "\n   - If you bust (exceeds 21), the dealer wins regardless of the dealer's hand." +
+                            "\n   - If the dealer busts, you win." +
+                            "\n   - If both the player (you) and dealer have the same hand value, it's a push (a tie), and your bet is returned." +
+                            "\n6. BLACKJACK: If the player (you) or dealer is dealt 21 from the start (Ace & a 10-value card), player/dealer got a blackjack" , "BLACKJACK'S BASIC GAMEPLAY",
                     JOptionPane.INFORMATION_MESSAGE);
         }
         else if(selectedButton == btnInfo) {
-            playSE(".//restest//click.wav");
+            playSE(".//res//chips.wav");
             JOptionPane.showMessageDialog(this, "Product of Driss dep trai va nhung nguoi ban" +
                     "\n:>>>>>>>>", "INFORMATION", JOptionPane.INFORMATION_MESSAGE);
         }
     }
+    public void playSE(String Sound) {
 
-        public void playSE(String Sound) {
+        SE.setFile(Sound);
+        SE.play();
+    }
 
-            SE.setFile(Sound);
-            SE.play();
-        }
 }
