b28e762a113badbd2315e401f3cf8c5cf9012bbf
Thanhquan2601 <145652317+Thanhquan2601@users.noreply.github.com>
2023-12-21 20:39:01 +0700

    Update Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -1,18 +1,18 @@
-import java.util.ArrayList;
-//import java.util.Collection;
-import java.util.Collections;
+import java.util.ArrayList,
+//import java.util.Collection,
+import java.util.Collections,
 
 public class Deck {
 
-    private ArrayList<Card> deck;
+    private ArrayList<Card> deck,
 
     public Deck(){
-        deck = new ArrayList<Card>();
+        deck = new ArrayList<Card>(),
         for(int i = 0; i < 4; i++){
             for(int j = 0; j < 13; j++){
                 if(j==0){
                     Card card = new Card(i,j,11); //11 la ace
-                    deck.add(card);
+                    deck.add(card),
                 }
                 else if(j >= 10){
                     Card card = new Card(i,j, 10); // 10 la JQK
